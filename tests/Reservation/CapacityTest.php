9d7ff1862036e0430194b7bdbc2f839daf1bffe7
Jeroen de Haar <jeroen@deviced.nl>
2023-03-14 12:20:27 +0100

    Prefer stable OS versions and rename test to comply with it's test case

--- tests/Reservation/CapacityTest.php
+++ tests/Reservation/CapacityTest.php
@@ -8,11 +8,11 @@ use MetMax\Cbr\Reservation\Type\CapacityResponse;
 use MetMax\Cbr\Tests\AbstractTestCase;
 use MetMax\Cbr\Tests\Traits\InteractWithReservation;
 
-class RequestsTest extends AbstractTestCase
+class CapacityTest extends AbstractTestCase
 {
     use InteractWithReservation;
 
-    public function test_it_returns_requests(): void
+    public function test_it_returns_capacity(): void
     {
         $response = $this->reservationClient()->findCapacity(new CapacityRequest(
             new DateTime('11-03-2023'),
